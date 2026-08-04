70df54898a56c2479b175b5ca8eb6f091b0c5e60
Marcusvgoncalves <marcusvg23@gmail.com>
2026-08-04 15:11:06 -0300

    rebuild para Next.js

--- db/migrate/20260801000002_add_auth_fields_to_iam_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAuthFieldsToIamUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :iam_users, :password_digest, :string
-    add_column :iam_users, :mfa_secret, :string
-    add_column :iam_users, :mfa_enabled, :boolean, default: false
-    add_column :iam_users, :reset_token, :string
-    add_column :iam_users, :reset_token_expires_at, :datetime
-  end
-end
--- db/migrate/20260801000003_add_mfa_setup_complete_to_iam_users.rb
+++
@@ -1,5 +0,0 @@
-class AddMfaSetupCompleteToIamUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :iam_users, :mfa_setup_complete, :boolean, default: false
-  end
-end
